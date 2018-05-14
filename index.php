b55f205a5914d105ebe82c6010b3e0a1d39e6eed
amhaualamar@orange.fr <Amar11>
2018-05-14 19:10:53 +0200

    php

---
+++ index.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Exercice1</title>
+  <meta charset="UTF-8">
+</head>
+<body>
+  <?php
+
+  $nom = "Amh";
+  print "$nom";
+
+?>
+<hr>
+<?php
+  $nom = "Amh";
+  $prenom ="Amar";
+  $age = 30;
+  echo "$nom $prenom $age";
+  ?>
+
+
+  <hr>
+  <?php
+$km = "1 ";
+echo "$km";
+$km = "3 ";
+echo  "$km";
+$km = "125 ";
+echo  "$km";
+
+  ?>
+  <hr>
+  <?php
+  $string ="hello";
+  $int =1;
+  $float = 1.5;
+  $bolean = true;
+echo "$string $int $float $bolean";
+?>
+<hr>
+<?php
+$int ="";
+echo "$int";
+$int ="bonjour";
+echo "$int";
+?>
+<hr>
+<?php
+$nom = "amar";
+echo " bonjour,".$nom." comment vas tu?";
+ ?>
+ <hr>
+ <?php
+ $nom = "Amh";
+ $prenom ="Amar";
+ $age = 30;
+ echo "bonjour, ".$nom ." " .$prenom. " ".$age." ans";
+  ?>
+  <hr>
+  <?php
+  $nom = "Amh";
+  $prenom ="Amar";
+  $age = 30;
+  echo "bonjour ".$nom ." ".$prenom .",tu as ". $age." ans.";
+  ?>
+  <hr>
+  <?php
+  $operation1 = 3 + 4;
+  $operation2 = 5 * 20;
+  $operation3 = 45 / 5;
+  echo $operation1." ".$operation2." ".$operation3;
+  ?>
