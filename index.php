42701564f25828ee8fbf18f35123691912d0373b
amhaualamar@orange.fr <Amar11>
2018-05-14 18:25:11 +0200

    php

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Exercice1</title>
+  <meta charset="UTF-8">
+</head>
+<body>
+  <?php
+
+  $nom = "Amh";
+  print "$nom";
+
+?>
+<hr>
+<?php
+  $nom = "Amh";
+  $prenom ="Amar";
+  $age = 30;
+  echo "$nom $prenom $age";
+  ?>
+
+
+  <hr>
+  <?php
+$km = "1 ";
+echo "$km";
+$km = "3 ";
+echo  "$km";
+$km = "125 ";
+echo  "$km";
+
+  ?>
+  <hr>
+  <?php
+
+
+  ?>
